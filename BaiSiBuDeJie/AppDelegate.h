2c13132cb7a793d6dbfa3b4c1a84ebe84ff57b54
Jiang <Jiang@JiangdeMacBook-Pro.local>
2016-07-18 15:27:28 +0800

    项目创建完毕

---
+++ BaiSiBuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by Jiang on 16/7/18.
+//  Copyright © 2016年 Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
